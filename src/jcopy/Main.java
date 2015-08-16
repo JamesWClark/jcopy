1927bc1181723a25900ad96003b53b4dcc586e4c
JamesWClark <jwclark@rockhursths.edu>
2015-08-15 23:02:19 -0500

    combine files from commons io project

--- src/jcopy/Main.java
+++ src/jcopy/Main.java
@@ -1,49 +1,82 @@
 /**
  * What does jcopy do?
- * 
+ *
  * It's a command line application with commands like this...
- * 		
- * 
+ *
+ *
  */
 package jcopy;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 
 import org.apache.commons.io.FileUtils;
+import ui.MainGUI;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-		Options options = new Options(args);
-		if(options.list != null) {
-			InputStream is = new FileInputStream(options.list);
-            InputStreamReader isr = new InputStreamReader(is, Charset.forName("UTF-8"));
-            BufferedReader br = new BufferedReader(isr);
-            String line = "";
-            while((line = br.readLine()) != null) {
-            	try {
-	            	String source = options.source.getAbsolutePath();
-	            	String destination = "\\\\" + line + "\\" + options.destination;
-	            	System.out.println("copying from: " + source);
-	            	System.out.println("copying to: " + destination);
-	            	long start = System.currentTimeMillis();
-	            	FileUtils.copyFile(options.source, new File(destination));
-	            	long finish = System.currentTimeMillis();
-	            	System.out.println("finished copying to " + line + " in " + (finish-start) + " ms\n");
-            	} catch (Exception ex) {
-                	System.out.println("failed: " + line);
-                	ex.printStackTrace();
-                	System.out.println();
+    public static void main(String args[]) {
+        //GUI
+        if (args.length < 1) {
+            try {
+                for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                    if ("Nimbus".equals(info.getName())) {
+                        javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                        break;
+                    }
                 }
+            } catch (ClassNotFoundException ex) {
+                java.util.logging.Logger.getLogger(MainGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (InstantiationException ex) {
+                java.util.logging.Logger.getLogger(MainGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (IllegalAccessException ex) {
+                java.util.logging.Logger.getLogger(MainGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+                java.util.logging.Logger.getLogger(MainGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
             }
-		} else {
-			FileUtils.copyDirectory(options.source, options.destination);
-		}
-	}
+            java.awt.EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                    new MainGUI().setVisible(true);
+                }
+            });
+        //COMMAND LINE
+        } else {
+            try {
+                Options options = new Options(args);
+                if (options.list != null) {
+                    InputStream is = new FileInputStream(options.list);
+                    InputStreamReader isr = new InputStreamReader(is, Charset.forName("UTF-8"));
+                    BufferedReader br = new BufferedReader(isr);
+                    String line = "";
+                    while ((line = br.readLine()) != null) {
+                        try {
+                            String source = options.source.getAbsolutePath();
+                            String destination = "\\\\" + line + "\\" + options.destination;
+                            System.out.println("copying from: " + source);
+                            System.out.println("copying to: " + destination);
+                            long start = System.currentTimeMillis();
+                            FileUtils.copyFile(options.source, new File(destination));
+                            long finish = System.currentTimeMillis();
+                            System.out.println("finished copying to " + line + " in " + (finish - start) + " ms\n");
+                        } catch (Exception ex) {
+                            System.out.println("failed: " + line);
+                            ex.printStackTrace();
+                            System.out.println();
+                        }
+                    }
+                } else {
+                    FileUtils.copyDirectory(options.source, options.destination);
+                }
+            } catch (FileNotFoundException ex) {
+                System.out.println("\n"+ex.getMessage()+"\n");
+            } catch (IOException ex) {
+                System.out.println("\n"+ex.getMessage()+"\n");                
+            }
+        }
+    }
 }
--- src/ui/MainGUI.java
+++ src/ui/MainGUI.java
@@ -13,7 +13,6 @@ public class MainGUI extends javax.swing.JFrame {
     public MainGUI() {
         initComponents();
         lstNetbiosNames.setTransferHandler(new TxtTransferHandler(lstNetbiosNames));
-        
     }
 
     @SuppressWarnings("unchecked")
@@ -201,32 +200,6 @@ public class MainGUI extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_btnCopyActionPerformed
 
-    public static void main(String args[]) {
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MainGUI().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnChooseDestination;
